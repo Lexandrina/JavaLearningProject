6e0d5000c5f392f327dfe7ace5bfb0dde5d48bc9
alexandrina <allexy.revenco@gmail.com>
2024-05-09 23:25:13 +0300

    Added the implementation of file management service that will be used for the ATF

---
+++ src/main/java/md/tekwillacademy/filemanagementservice/FileTaskExecution.java
@@ -0,0 +1,7 @@
+package md.tekwillacademy.filemanagementservice;
+
+public class FileTaskExecution {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/md/tekwillacademy/filemanagementservice/FilesManager.java
@@ -0,0 +1,4 @@
+package md.tekwillacademy.filemanagementservice;
+
+public class FilesManager {
+}
